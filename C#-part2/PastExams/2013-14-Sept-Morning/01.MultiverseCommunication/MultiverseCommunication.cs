534c0dd080f04f11460be210a9a9281bb3c734ee
Martin Ivanov Matev <martin.matev89@gmail.com>
2015-02-21 19:33:40 +0200

    1/5

---
+++ C#-part2/PastExams/2013-14-Sept-Morning/01.MultiverseCommunication/MultiverseCommunication.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MultiverseCommunication
+{
+    static void Main()
+    {
+        var alphabet = new List<string> { "CHU", "TEL", "OFT", "IVA", "EMY", "VNB", "POQ", "ERI", "CAD", "K-A", "IIA", "YLO", "PLA" };
+        string input = Console.ReadLine();
+
+        long decimalRepresentation = 0;
+        for (int i = 0; i < input.Length; i += 3)
+        {
+            string digitIn13 = input.Substring(i, 3);
+            int decimalValue = alphabet.IndexOf(digitIn13);
+
+            decimalRepresentation *= 13;
+            decimalRepresentation += decimalValue;
+        }
+        Console.WriteLine(decimalRepresentation);
+    }
+}
