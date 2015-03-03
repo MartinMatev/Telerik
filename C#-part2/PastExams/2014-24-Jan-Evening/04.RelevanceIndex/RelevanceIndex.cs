141e042aecc11e4fecabb3d11170d3782cd88f56
Martin Ivanov Matev <martin.matev89@gmail.com>
2015-03-03 13:58:35 +0200

    3/5

---
+++ C#-part2/PastExams/2014-24-Jan-Evening/04.RelevanceIndex/RelevanceIndex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+class RelevanceIndex
+{
+    static void Main()
+    {
+        string searchWord = Console.ReadLine();
+        int paragraphsNum = int.Parse(Console.ReadLine());
+        string[] text = new string[paragraphsNum];
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+
+        for (int i = 0; i < paragraphsNum; i++)
+        {
+            string[] currentLine = Console.ReadLine().Split(new char[] { ' ', '.', ',', '(', ')', ';', '-', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            int relevanceIndexCount = 0;
+
+            for (int j = 0; j < currentLine.Length; j++)
+            {
+                if (currentLine[j].ToLower() == searchWord.ToLower())
+                {
+                    relevanceIndexCount++;
+                    currentLine[j] = currentLine[j].ToUpper();
+                }
+            }
+            text[i] = string.Join(" ", currentLine);
+            dict.Add(text[i], relevanceIndexCount);
+        }
+        var result = dict.OrderByDescending(x => x.Value);
+        foreach (var line in result)
+        {
+            Console.WriteLine("{0}", line.Key);
+        }
+        
+    }
+}
