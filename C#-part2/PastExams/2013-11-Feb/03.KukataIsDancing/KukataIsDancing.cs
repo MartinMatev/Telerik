20ba79ba444bc56277b47d700d77301ef4e767e9
Martin Ivanov Matev <martin.matev89@gmail.com>
2015-03-04 19:04:38 +0200

    4/5

---
+++ C#-part2/PastExams/2013-11-Feb/03.KukataIsDancing/KukataIsDancing.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class KukataIsDancing
+{
+    static void Main()
+    {
+        // Read the input
+        int numberOfDances = int.Parse(Console.ReadLine());
+        string[,] floor = {
+                              {"RED", "BLUE", "RED"},
+                              {"BLUE", "GREEN", "BLUE"},
+                              {"RED", "BLUE", "RED"}
+                          };
+
+        int[][] directions = {
+                                 new int[]{0, 1},   // right
+                                 new int[]{-1, 0},  // up
+                                 new int[]{0, -1},  // left
+                                 new int[]{1, 0}    // down
+                             };
+
+        for (int i = 0; i < numberOfDances; i++)
+        {
+            string dance = Console.ReadLine();
+            int[] pos = { 1, 1 };   //starting position
+            int dirIndex = 0;
+            int[] currentDir = directions[dirIndex];
+
+            foreach (char step in dance)
+            {
+                switch (step)
+                {
+                    case 'W':
+                        pos = Walk(pos, currentDir);
+
+                        // Rotate the dance flor
+                        for (int j = 0; j <= 1; j++)
+                        {
+                            if (pos[j] >= floor.GetLength(j))
+                            {
+                                pos[j] = 0;
+                            }
+                            else if (pos[j] < 0)
+                            {
+                                pos[j] = floor.GetLength(j) - 1;
+                            }
+                        }
+                        break;
+                    case 'R':
+                        dirIndex = TurnRight(directions, dirIndex);
+                        currentDir = directions[dirIndex]; // assign new direction
+                        break;
+                    case 'L':
+                        dirIndex = TurnLeft(directions, dirIndex);
+                        currentDir = directions[dirIndex]; // assign new direction
+                        break;
+                }
+            }
+
+            int currentRow = pos[0];
+            int currentCol = pos[1];
+            string result = floor[currentRow, currentCol];
+
+            Console.WriteLine(result);
+        }
+    }
+
+    private static int[] Walk(int[] pos, int[] currentDir)
+    {
+        for (int i = 0; i < pos.GetLength(0); i++)
+        {
+            pos[i] += currentDir[i];
+        }
+        return pos;
+    }
+
+    private static int TurnLeft(int[][] directions, int dirIndex)
+    {
+        dirIndex += 1;
+        if (dirIndex >= directions.GetLength(0))
+        {
+            dirIndex = 0;
+        }
+        return dirIndex;
+    }
+
+    private static int TurnRight(int[][] directions, int dirIndex)
+    {
+        dirIndex -= 1;
+        if (dirIndex < 0)
+        {
+            dirIndex = directions.GetLength(0) - 1;
+        }
+        return dirIndex;
+    }
+}
---
+++ C#-part2/PastExams/2013-11-Feb/04.FakeTextMarkupLanguage/FakeTextMarkupLanguage.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+    class FakeTextMarkupLanguage
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string currentLine = Console.ReadLine();
+            int closeTagStartIndex = currentLine.IndexOf("</");
+
+            while (closeTagStartIndex != -1)
+            {
+                int closeTagEndIndex = currentLine.IndexOf(">", closeTagStartIndex + 1);
+                string tagName = currentLine.Substring(closeTagStartIndex + 2, closeTagEndIndex - closeTagStartIndex - 2);
+
+                int openTagStart = currentLine.LastIndexOf(string.Format("<{0}>", tagName), closeTagStartIndex);
+                int openTagEnd = openTagStart + tagName.Length + 2;
+                string content = currentLine.Substring(openTagEnd, closeTagStartIndex - openTagEnd);
+
+                switch (tagName)
+                {
+                    case "rev":
+                        content = Reverse(content);
+                        break;
+                    case "toggle":
+                        content = Toggle(content);
+                        break;
+                    case "lower":
+                        content = content.ToLower();
+                        break;
+                    case "upper":
+                        content = content.ToUpper();
+                        break;
+                    case "del":
+                        content = string.Empty;
+                        break;
+                    default:
+                        break;
+                }
+                // Remove the already used opening and closing LAST tag (last in, first out) and add the new
+                // (reworked) content, depending on the tag that changed it
+                currentLine = currentLine.Remove(openTagStart, closeTagEndIndex - openTagStart + 1);
+                currentLine = currentLine.Insert(openTagStart, content);
+
+                closeTagStartIndex = currentLine.IndexOf("</");
+            }
+            Console.WriteLine(currentLine);
+        }
+    }
+
+    private static string Toggle(string content)
+    {
+        StringBuilder result = new StringBuilder();
+
+        foreach (var letter in content)
+        {
+            if (char.IsLetter(letter))
+            {
+                if (char.IsLower(letter))
+                {
+                    result.Append(char.ToUpper(letter));
+                }
+                else
+                {
+                    result.Append(char.ToLower(letter));
+                }
+            }
+            else
+            {
+                result.Append(letter);
+            }
+        }
+
+        return result.ToString();
+    }
+
+    private static string Reverse(string content)
+    {
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = content.Length - 1; i >= 0 ; i--)
+        {
+            reversed.Append(content[i]);
+        }
+
+        return reversed.ToString();
+    }
+}
