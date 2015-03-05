c2569c013da1174b0004ff52e93cd93f4270771e
Martin Ivanov Matev <martin.matev89@gmail.com>
2015-03-05 17:52:05 +0200

    4/5

---
+++ C#-part2/PastExams/2013-5-Feb/03.Laser/Laser.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Laser
+{
+    static void Main()
+    {
+        string[] line = Console.ReadLine().Split(' ');
+        int cubeWidth = int.Parse(line[0]);
+        int cubeHeight = int.Parse(line[1]);
+        int cubeDepth = int.Parse(line[2]);
+
+        bool[, ,] cube = new bool[cubeWidth, cubeHeight, cubeDepth];
+
+        MarkBurnt(cubeWidth, cubeHeight, cubeDepth, cube);
+
+        line = Console.ReadLine().Split(' ');   // - 1 because it starts form 1,1,1 and not 0,0,0
+        int nextWidth = int.Parse(line[0]) - 1;
+        int nextHeight = int.Parse(line[1]) - 1;
+        int nextDepth = int.Parse(line[2]) - 1;
+
+        line = Console.ReadLine().Split(' ');
+        int dirWidth = int.Parse(line[0]);
+        int dirHeight = int.Parse(line[1]);
+        int dirDepth = int.Parse(line[2]);
+
+        // shoot laser
+        int currentWidth, currentHeight, currentDepth;
+        while (true)
+        {
+            // go to the next position
+            currentWidth = nextWidth;
+            currentHeight = nextHeight;
+            currentDepth = nextDepth;
+
+            // burn the current position
+            cube[currentWidth, currentHeight, currentDepth] = true;
+
+            // check next position
+
+            if (nextWidth + dirWidth < 0 || nextWidth + dirWidth >= cube.GetLength(0))
+            {
+                dirWidth *= -1;
+            }
+            if (nextHeight + dirHeight < 0 || nextHeight + dirHeight >= cube.GetLength(1))
+            {
+                dirHeight *= -1;
+            }
+            if (nextDepth + dirDepth < 0 || nextDepth + dirDepth >= cube.GetLength(2))
+            {
+                dirDepth *= -1;
+            }
+
+
+            nextWidth += dirWidth;
+            nextHeight += dirHeight;
+            nextDepth += dirDepth;
+
+            if (cube[nextWidth, nextHeight, nextDepth])
+            {
+                break;
+            }
+        }
+
+        // print current position
+        Console.WriteLine("{0} {1} {2}", currentWidth + 1, currentHeight + 1, currentDepth + 1);
+    }
+
+    private static void MarkBurnt(int cubeWidth, int cubeHeight, int cubeDepth, bool[, ,] cube)
+    {
+        for (int w = 0; w < cubeWidth; w++)
+        {
+            cube[w, 0, 0] = true;
+            cube[w, cubeHeight - 1, 0] = true;
+            cube[w, 0, cubeDepth - 1] = true;
+            cube[w, cubeHeight - 1, cubeDepth - 1] = true;
+        }
+
+        for (int d = 0; d < cubeDepth; d++)
+        {
+            cube[0, 0, d] = true;
+            cube[cubeWidth - 1, 0, d] = true;
+            cube[0, cubeHeight - 1, d] = true;
+            cube[cubeWidth - 1, cubeHeight - 1, d] = true;
+        }
+
+        for (int h = 0; h < cubeHeight; h++)
+        {
+            cube[0, h, 0] = true;
+            cube[cubeWidth - 1, h, 0] = true;
+            cube[0, h, cubeDepth - 1] = true;
+            cube[cubeWidth - 1, h, cubeDepth - 1] = true;
+        }
+    }
+}
---
+++ C#-part2/PastExams/2013-5-Feb/04.Brackets/Brackets.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Brackets
+{
+    static void Main()
+    {
+        // Read the input
+        int n = int.Parse(Console.ReadLine());
+        string indent = Console.ReadLine();
+        string[] lines = new string[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            lines[i] = Console.ReadLine();
+        }
+
+        char prevCh = new char();
+        int indentCount = 0;
+        List<string> code = new List<string>();
+
+        for (int i = 0; i < n; i++)
+        {
+            StringBuilder line = new StringBuilder();
+            string lineIndent = GetLineIndent(indent, indentCount); // the indent, concatenated indentCount times
+
+            foreach (char ch in lines[i])
+            {
+                if (ch == '{')
+                {
+                    AddLine(line, lineIndent, code);
+                    line.Clear();
+                    code.Add(string.Format("{0}{1}", lineIndent, ch));
+                    indentCount++;
+                    lineIndent = GetLineIndent(indent, indentCount);
+                }
+                else if (ch == '}')
+                {
+                    AddLine(line, lineIndent, code);
+                    line.Clear();
+                    indentCount--;
+                    lineIndent = GetLineIndent(indent, indentCount);
+                    code.Add(string.Format("{0}{1}", lineIndent, ch));
+                }
+                else
+                {
+                    if (!(ch == ' ' && prevCh == ' '))
+                    {
+                        line.Append(ch);
+                    }
+                }
+
+                prevCh = ch;
+            }
+
+            AddLine(line, lineIndent, code);
+        }
+
+        foreach (string line in code)
+        {
+            Console.WriteLine(line);
+        }
+    }
+
+    private static void AddLine(StringBuilder line, string lineIndent, List<string> code)
+    {
+        if (line.ToString().TrimStart().Length > 0)
+        {
+            code.Add(string.Format("{0}{1}", lineIndent, line.ToString().TrimStart()));
+        }
+    }
+
+    private static string GetLineIndent(string indent, int indentCount)
+    {
+        string lineIndent = string.Empty;
+
+        for (int i = 0; i < indentCount; i++)
+        {
+            lineIndent += indent;
+        }
+
+        return lineIndent;
+    }
+}
