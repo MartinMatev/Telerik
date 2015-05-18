42238aceb503e468e6f61fec4033e35b844bb567
Martin Ivanov Matev <martin.matev89@gmail.com>
2015-05-18 21:36:02 +0300

    JS first assignment

---
+++ JavaScriptFundamentals/Homework/01.FirstHomework/FirstHomework/02.QuotedString.js
@@ -0,0 +1,6 @@
+﻿/* Problem 2. Quoted text
+Create a string variable with quoted text in it.
+For example: `'How you doin'?', Joey said'
+*/
+var string = '"How you doin?", Joey said.';
+console.log(string);
