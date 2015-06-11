4bf9fc6337f1b34af332240289576ce0dc8acb2d
Martin Matev <martin.matev89@gmail.com>
2015-06-11 08:42:17 +0300

    Exam prep

--- JavaScriptFundamentals/Exam-Preparation/2014-20-May-Evening/02.Horsy/Horsy.js
+++ JavaScriptFundamentals/Exam-Preparation/2014-20-May-Evening/02.Horsy/Horsy.js
@@ -1,5 +1,54 @@
 function solve(input){
+    var rowsAndCols = input[0].split(' ').map(Number);
+    var rows = rowsAndCols[0];
+    var cols = rowsAndCols[1];
+    var row = rows - 1;
+    var col =  cols - 1;
+    var moves = [
+        [-2, 1],
+        [-1, 2],
+        [1, 2],
+        [2, 1],
+        [2, -1],
+        [1, -2],
+        [-1, -2],
+        [-2, -1]
+    ];
 
+    var pointsSum = 0;
+    var moveCount = 0;
+    var used = [];
+
+    while(true){
+        // out of the play field
+        if (row < 0 || row >= rows
+        || col < 0 || col >= cols){
+            return 'Go go Horsy! Collected ' + pointsSum + ' weeds';
+        }
+
+        if(used[row + ' ' + col]){
+            return 'Sadly the horse is doomed in ' + moveCount + ' jumps';
+        }
+
+        moveCount += 1;
+        pointsSum += getPoints(row, col);
+
+        var move = moves[getValue(input, row, col) - 1];
+
+        used[row + ' ' + col] = true;
+        row += move[0];
+        col += move[1];
+    }
+
+    function getPoints(row, col){
+
+        return Math.pow(2, row) - col;
+    }
+
+    function getValue(input, row, col){
+
+        return input[row + 1][col];
+    }
 }
 
 argsOne = [
@@ -16,5 +65,5 @@ argsTwo = [
     '52188'
 ];
 
-solve(argsOne);
-solve(argsTwo);
+console.log(solve(argsOne));
+console.log(solve(argsTwo));
